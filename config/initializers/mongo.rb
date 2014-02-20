1d2621880b49d2ae95e40db9950a21b34753b8d1
Koutarou Chikuba <miz404@gmail.com>
2014-02-20 16:48:03 +0900

    Add mongolab settings

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,5 +1,16 @@
-MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
-MongoMapper.database = "#myapp-#{Rails.env}"
+if ENV['MONGOLAB_URI']
+  regex_match = /.*:\/\/(.*):(.*)@(.*):(.*)\//.match(ENV['MONGOLAB_URI'])
+  host = regex_match[3]
+  port = regex_match[4]
+  db_name = regex_match[1]
+  pw = regex_match[2]
+  MongoMapper.connection = Mongo::Connection.new(host, port)
+  MongoMapper.database = db_name
+  MongoMapper.database.authenticate(db_name, pw)
+else
+  MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+  MongoMapper.database = "#myapp-#{Rails.env}"
+end
 
 if defined?(PhusionPassenger)
   PhusionPassenger.on_event(:starting_worker_process) do |forked|
